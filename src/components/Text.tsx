c3bdd607222d26a8b665b5dfbe010ae54c501199
SM <52240990+baesumin@users.noreply.github.com>
2024-09-04 10:20:22 +0900

    feat: element별 props타입추가

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 
+type TextProps<C extends React.ElementType> = {
+  as?: C;
+  children: ReactNode;
+} & React.ComponentPropsWithoutRef<C>;
+
 export const Text = <C extends React.ElementType>({
   as,
   children,
-}: {
-  as?: C;
-  children: ReactNode;
-}) => {
+}: TextProps<C>) => {
   const Component = as || "span";
   return <Component>{children}</Component>;
 };
