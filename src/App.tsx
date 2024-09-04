0044b91d8fadf6c178c4f84a02233038c21efe53
SM <52240990+baesumin@users.noreply.github.com>
2024-09-04 11:18:15 +0900

    feat: * 색상 및 스타일 전달 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import { Text } from "./components";
+import React from "react";
+
+const Emphasis = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <em style={{ background: "yellow", color: "black", fontSize: "40px" }}>
+      {children}
+    </em>
+  );
+};
 
 function App() {
   return (
-    <>
-      <Text as={"h1"}>hello</Text>
+    <div className="App">
+      <Text as={"h1"} color="violet" style={{ backgroundColor: "black" }}>
+        hello
+      </Text>
       <Text as={"h2"}>hello</Text>
       <Text>hello</Text>
-    </>
+      <br />
+      <Text as={Emphasis}>This is important. You are awesome!!!</Text>
+    </div>
   );
 }
 
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,15 +1,37 @@
 import { ReactNode } from "react";
 
+type Rainbow =
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "indigo"
+  | "violet";
+
 type TextProps<C extends React.ElementType> = {
   as?: C;
-  children: ReactNode;
-} & React.ComponentPropsWithoutRef<C>;
+  color?: Rainbow | "black";
+};
+
+type Props<C extends React.ElementType> = React.PropsWithChildren<
+  TextProps<C>
+> &
+  Omit<React.ComponentPropsWithoutRef<C>, keyof TextProps<C>>;
 
 export const Text = <C extends React.ElementType = "span">({
   as,
+  style,
+  color,
   children,
   ...restProps
-}: TextProps<C>) => {
+}: Props<C>) => {
   const Component = as || "span";
-  return <Component {...restProps}>{children}</Component>;
+
+  const internalStyles = color ? { style: { ...style, color } } : {};
+  return (
+    <Component {...restProps} {...internalStyles}>
+      {children}
+    </Component>
+  );
 };
